559e1bceaba5bb801298cd64c45cb55c839f700f
kiranps25 <chn24mc066@ceconline.edu>
2025-03-03 15:53:44 +0530

    Update matrixadd.java

--- matrixadd.java
+++ matrixadd.java
@@ -43,10 +43,33 @@ class matrixadd
 			System.out.println("");
 		}
 	}
+	void sym()
+	{
+	int f=0;
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(a[i][j]!=a[j][i])
+				{
+					f=1;
+				}
+			}
+		}
+		if(f==1)
+		{
+			System.out.println("not symmetric");
+		}
+		else
+		{
+			System.out.println("symmetric");
+		}
+	}
 	public static void main(String args[])
 	{
 		matrixadd k=new matrixadd();
 		k.insert();
 		k.add();
+		k.sym();
 	}
 }
